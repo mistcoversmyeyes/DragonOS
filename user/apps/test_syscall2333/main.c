8b3f13ea49a65e433e932072b99f80121860b5f9
Yuming <mingjiangyu1@qq.com>
2025-07-16 11:32:50 +0800

    feat(user/apps/):添加了验证系统调用是否成功添加的用户程序

--- user/apps/test_syscall2333/main.c
+++ user/apps/test_syscall2333/main.c
@@ -5,7 +5,6 @@
 int main() {
     printf("Testing custom syscall 2333...\n");
     
-    // Call our custom syscall 2333
     long result = syscall(2333);
     
     printf("Successfully called syscall 2333, return value is: %ld\n", result);
